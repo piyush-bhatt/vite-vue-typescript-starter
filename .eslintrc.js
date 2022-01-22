970ab2f3ca42ec909411f9b99e11bedc91fabf80
Piyush Bhatt <gavinharris123@gmail.com>
2022-01-22 14:34:55 +0100

    add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  // https://eslint.org/docs/rules/no-undef#nodejs
+  env: {
+    node: true,
+  },
+  parser: '@typescript-eslint/parser',
+  plugins: [
+    '@typescript-eslint',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+}
+
